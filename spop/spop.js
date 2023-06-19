1cc2780a50f6299baf3dd2d6bc59ddd69b5e3a41
Gshihao <100885352+Gshihao@users.noreply.github.com>
2023-06-19 16:11:07 +0800

    Update spop.js

--- spop/spop.js
+++ spop/spop.js
@@ -5,16 +5,19 @@
    @copyright: SHAO
 */
    (function (){
-      let Ret='<div class="AlertOverride_a" id="AlertOverridea"><div class="AlertContenxt"><div class="AlertContenxtCon">';
-          Ret+=' <p id="cuowuAlertOverridea"></p></div><div class="AlertContenxtConddjjd"><button class="AlertContenxtBon" onclick="" id="dkdkjdjfjjfjfjf">确定</button></div>';
-          Ret+='</div></div>';
-          document.body.innerHTML+=Ret;
-    }())
-    let spop=(context)=>{
-       document.getElementById("AlertOverridea").style.display="block";
-       document.getElementById("cuowuAlertOverridea").innerText=context;
-      
-       document.getElementById("dkdkjdjfjjfjfjf").onclick=function (){
-         document.getElementById("AlertOverridea").style.display="none";
-       }
-      }
+         let Html1a='	<div class="choiceBox" id="choiceBoxA" ><div class="choiceXBox" ><div class="choiceXBox_F2">';
+             Html1a+=' <div class="choiceBox_box11" onclick="CloseBottom()"></div><!-- 内容--><div class="choiceBox_box12" id="choiceBoxboxid">';
+             Html1a+='</div><div class="choiceBox_box12Top"></div><!--页面数据展示顶部遮罩层--><div class="choiceBox_box12Botyom">';
+             Html1a+='</div><!--页面数据展示顶部遮罩层--></div></div></div> ';
+         document.body.innerHTML+=Html1a;
+      }());// 动态对html进行注入框架
+       var spop=(context)=>{
+                 let choiceBoxA= document.getElementById("choiceBoxA");
+                  choiceBoxA.style.display="block";
+                  choiceBoxA.style.animation="showtop 0.5s forwards";
+                  document.getElementById("choiceBoxboxid").innerHTML=context;
+                 };
+        var CloseBottom=()=>{
+                 let choiceBoxA= document.getElementById("choiceBoxA");
+                 choiceBoxA.style.animation="CloseBottom 1s forwards";
+        }
